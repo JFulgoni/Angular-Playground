9952e9af5e0ef878e0e36c0bc3b8bee4780b2d0a
jfulgoni@github.com <jsf2154@columbia.edu>
2020-08-11 08:31:17 -0400

    Modified landing page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
 
   username = '';
   password = '';
+  email = '';
 
   constructor(private router: Router) { }
 
@@ -18,13 +19,17 @@ export class LoginComponent implements OnInit {
 
   login() {
     // validation of username and password would occur here
-    if (this.allValuesPresent()) {
+    if (this.isEmail()) {
       this.router.navigateByUrl('/actions');
     }
   }
 
   allValuesPresent() {
     return this.validateString(this.username) && this.validateString(this.password);
+  }
+
+  isEmail():boolean {
+    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(this.email);
   }
 
   validateString(string: String) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '27bd0596cef5195742b5db0984b4c7ee',
+  corsProxy: '',
+  apiURL: 'https://classic.warcraftlogs.com:443/v1/',
 };
