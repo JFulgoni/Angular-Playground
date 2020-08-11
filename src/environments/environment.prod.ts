5a1e73129e51ac3a95c0477b6334e2af6da67497
jfulgoni@github.com <jsf2154@columbia.edu>
2020-08-11 08:56:21 -0400

    Adding CORS URL to Prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiKey: '27bd0596cef5195742b5db0984b4c7ee',
-  corsProxy: '',
+  corsProxy: 'https://cors-anywhere.herokuapp.com/',
   apiURL: 'https://classic.warcraftlogs.com:443/v1/',
 };
